a615524bb799e323700a85d9796f66fb82063b33
Anshul Vishwakarma <72655193+AnshulWebDev@users.noreply.github.com>
2023-08-26 21:01:42 +0530

    Update Clients.jsx

--- components/Clients.jsx
+++ components/Clients.jsx
@@ -1,22 +1,53 @@
 "use client";
 
 import Link from "next/link";
-import { useState, createContext, useContext } from "react";
+import { useState, createContext, useContext, useEffect } from "react";
+import { Toaster } from "react-hot-toast";
+import axios from "axios";
+import { toast } from "react-hot-toast";
+export const Context = createContext({ user: {} });
 
-const Context = createContext({ user: {} });
 export const Provider = ({ children }) => {
   const [user, setUser] = useState({});
+  const meAPICall=async()=>{
+    const response = await axios({
+      method: "post",
+      url: `/api/auth/me`,
+      withCredentials: true,
+    });
+    const data = response.data;
+    // console.log(data);
+    if (data.success) setUser(data.user);
+  }
+  useEffect(() => {
+    meAPICall();
+  }, []);
   return (
-    <Context.Provider value={{ user, setUser }}>{children}</Context.Provider>
+    <Context.Provider value={{ user, setUser }}>
+      {children}
+      <Toaster />
+    </Context.Provider>
   );
 };
 
 export const LogoutBtn = () => {
-  const logoutHandler = () => {
-    alert("logged out");
+  const { user, setUser } = useContext(Context);
+  const logoutHandler = async () => {
+    try {
+      const response = await axios({
+        method: "get",
+        url: "/api/auth/logout",
+      });
+      const data = response.data;
+      if (!data.success) return toast.error(data.message);
+      setUser({});
+      toast.success(data.message);
+    } catch (error) {
+      return toast.error("Something went wrong");
+    }
   };
-  const { user } = useContext(Context);
-  return user.id ? (
+
+  return user._id ? (
     <button className="btn" onClick={logoutHandler}>
       Logout
     </button>
@@ -25,14 +56,16 @@ export const LogoutBtn = () => {
   );
 };
 
-export const TodoButton = ({id,completed}) => {
-  const deleteHandler=(id)=>{
-    alert(`deleating ${id}`)
-  }
+export const TodoButton = ({ id, completed }) => {
+  const deleteHandler = (id) => {
+    alert(`deleating ${id}`);
+  };
   return (
     <>
-    <input type="checkbox" checked={completed}/>
-    <button className="btn" onClick={()=>deleteHandler(id)}>Delete</button>
+      <input type="checkbox" checked={completed} />
+      <button className="btn" onClick={() => deleteHandler(id)}>
+        Delete
+      </button>
     </>
-  )
-}
+  );
+};
