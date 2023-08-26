bbeb398f30ad7da358d9cd5b12e204a3c2fa1971
Anshul Vishwakarma <72655193+AnshulWebDev@users.noreply.github.com>
2023-08-26 23:45:47 +0530

    Create addTodoForm.jsx

---
+++ src/app/addTodoForm.jsx
@@ -0,0 +1,64 @@
+"use client";
+
+import axios from "axios";
+import { useContext, useState } from "react";
+import { toast } from "react-hot-toast";
+import { useRouter } from "next/navigation";
+import { Context } from "../../components/Clients";
+const addTodoForm = () => {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const {user}=useContext(Context)
+  const router=useRouter()
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios({
+        method: "post",
+        url: "/api/newtask",
+        data: {
+          title,
+          description,
+        },
+        withCredentials: true,
+      });
+      const data = response.data;
+      console.log(response);
+      if (data.success == true) {
+        toast.success(data.message);
+      } 
+      if(response=="Request failed with status code 402"){
+        toast.error(data.message)
+      }
+      router.refresh()
+      setTitle("")
+      setDescription("")
+    } catch (error) {
+      toast.error(error);
+    }
+  };
+  if(!user._id) return router.replace("/login")
+  return (
+    <div className="login">
+      <section>
+        <form onSubmit={submitHandler}>
+          <input
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            type="text"
+            placeholder="Task Title"
+          />
+          <input
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            type="text"
+            placeholder="Task Description"
+          />
+          <button type="submit">Add Task</button>
+        </form>
+      </section>
+    </div>
+  );
+};
+
+export default addTodoForm;
