b8c36c52f542aee88da083786d7ae68291d85326
Anshul Vishwakarma <72655193+AnshulWebDev@users.noreply.github.com>
2023-08-26 21:01:20 +0530

    Update route.js

--- src/app/api/getdeltask/route.js
+++ src/app/api/getdeltask/route.js
@@ -21,16 +21,16 @@ export const POST = async (req) => {
         { status: 402 }
       );
     }
-    console.log(_id);
-    const getAllTask = await Task.find({ user: _id });
-    console.log(getAllTask);
-    if (!getAllTask) {
+    // console.log(_id);
+    const task = await Task.find({ user: _id });
+    // console.log(getAllTask);
+    if (!task) {
       return NextResponse.json(
         { success: false, message: "No task found" },
         { status: 301 }
       );
     }
-    return NextResponse.json({ success: true, getAllTask }, { status: 200 });
+    return NextResponse.json({ success: true, task }, { status: 200 });
   } catch (error) {
     console.log(error.message);
     return NextResponse.json(
@@ -61,7 +61,6 @@ export const DELETE = async (req) => {
     }
     // console.log(_id);
     await Task.deleteOne(_id);
-
     return NextResponse.json(
       { success: true, message: "Task Delete Successfully" },
       { status: 200 }
