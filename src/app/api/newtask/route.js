8e750f214f95ff938d26f7f02e2c574eb0aefeea
Anshul Vishwakarma <72655193+AnshulWebDev@users.noreply.github.com>
2023-08-26 11:12:41 +0530

    Update route.js

--- src/app/api/newtask/route.js
+++ src/app/api/newtask/route.js
@@ -1,22 +1,45 @@
 import { NextResponse } from "next/server";
 import { connectDB } from "../../../../utils/features";
 import { Task } from "../../../../models/task";
+import { User } from "../../../../models/user";
 
 export const POST = async (req) => {
   try {
     await connectDB();
-    const { title, description, user } = await req.json();
-    await Task.create({
+    const { title, description } = await req.json();
+    const cookiesValue = await req.cookies.get("token")?.value;
+    if(!title||!description){
+      return NextResponse.json(
+        { success: false, message: "Please Fill all fields" },
+        { status: 402 }
+      );
+    }
+    if (!cookiesValue) {
+      return NextResponse.json(
+        { success: false, message: "Please login first" },
+        { status: 402 }
+      );
+    }
+    const userDetails = await User.findOne({ token: cookiesValue });
+    if (!userDetails) {
+      return NextResponse.json(
+        { success: false, message: "unauthorize access or login first" },
+        { status: 402 }
+      );
+    }
+
+    const newTask = await Task.create({
       title,
       description,
-      user,
+      user: userDetails._id,
     });
+    await newTask.save();
     return NextResponse.json(
       { success: true, message: "Task created" },
-      {status: 200}
+      { status: 200 }
     );
   } catch (error) {
     console.log(error.message);
-    return NextResponse.json({ success: false }, { status: 500 });
+    return NextResponse.json({ success: false  }, { status: 500 });
   }
 };
