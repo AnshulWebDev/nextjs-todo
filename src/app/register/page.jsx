02f80d6186c4422bc8b181726f4a01e94541aee3
Anshul Vishwakarma <72655193+AnshulWebDev@users.noreply.github.com>
2023-08-26 21:01:12 +0530

    Update page.jsx

--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -1,25 +1,72 @@
-"use client"
+"use client";
 
+import axios from "axios";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { toast } from "react-hot-toast";
 
 const page = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter();
+
+  const registerHandler = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios({
+        method: "post",
+        url: `/api/auth/register`,
+        data: {
+          name,
+          email,
+          password,
+        },
+      });
+      const data = response.data;
+      // console.log(data);
+      if (!data.success) return toast.error(data.message);
+      toast.success(data.message);
+      return router.replace("/login");
+    } catch (error) {
+      console.log(error.message);
+      return toast.error("Fill All Fields")
+    }
+  };
+
   return (
     <div className="login">
-        <section>
-            <form action="">
-            <input type="text" placeholder="Enter Your Name"/>
-                <input type="email" placeholder="Enter Your Email"/>
-                <input type="password" placeholder="Enter Your Password"/>
-                <button type="submit">Signup</button>
-                <p>Or</p>
-                <Link href={"/login"}>Already have account?</Link>
-            </form>
-        </section>
+      <section>
+        <form onSubmit={registerHandler}>
+          <input
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+            type="text"
+            placeholder="Enter Your Name"
+          />
+          <input
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+            type="email"
+            placeholder="Enter Your Email"
+          />
+          <input
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+            type="password"
+            placeholder="Enter Your Password"
+          />
+          <button type="submit">Signup</button>
+          <p>Or</p>
+          <Link href={"/login"}>Already have account?</Link>
+        </form>
+      </section>
     </div>
-  )
-}
+  );
+};
 export const metadata = {
-    title: "Register",
-    description: "This is Register Page for Todo app",
-  };
-export default page
+  title: "Register",
+  description: "This is Register Page for Todo app",
+};
+export default page;
