90a56d14349f32bbb47c290ff70a61e7ba2bb7de
Anshul Vishwakarma <72655193+AnshulWebDev@users.noreply.github.com>
2023-08-26 21:01:27 +0530

    Create route.js

---
+++ src/app/api/auth/me/route.js
@@ -0,0 +1,26 @@
+import { NextResponse } from "next/server";
+import { connectDB } from "../../../../../utils/features";
+import { User } from "../../../../../models/user";
+export const POST = async (req) => {
+  try {
+    await connectDB();
+    const cookiesValue = await req.cookies.get("token")?.value;
+    console.log(cookiesValue);
+    const user = await User.findOne({ token: cookiesValue });
+    if (!user) {
+      return NextResponse.json(
+        { success: false, message: "Please login first" },
+        { status: 402 }
+      );
+    }
+    user.password = undefined;
+    user.token = undefined;
+    return NextResponse.json({ success: true, user }, { status: 200 });
+  } catch (error) {
+    console.log(error.message);
+    return NextResponse.json(
+      { success: false, message: "Internal server error Try Again" },
+      { status: 500 }
+    );
+  }
+};
